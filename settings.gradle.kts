be8f73951de560190af9b49cbf4ccf35f8b6a261
davidwheeler123 <david.wheeler@cultureamp.com>
2025-10-02 08:44:19 +1000

    fix: Remove ability to load plugins from cultureamp repo, which isn't working

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,3 @@
-// Set up to download gradle plugins from GitHub Packages
-pluginManagement {
-  repositories {
-    maven {
-      name = "GitHubPackages"
-      url = uri("https://maven.pkg.github.com/cultureamp/*")
-      credentials {
-        username = System.getenv("USERNAME") ?: extra["gpr.user"] as String
-        password = System.getenv("PACKAGE_READ_TOKEN") ?: extra["gpr.key"] as String
-      }
-    }
-    gradlePluginPortal()
-  }
-}
-
 apply(from = "get-name-and-version.gradle.kts")
 
 rootProject.name = gradle.extra["package_name"]!! as String
