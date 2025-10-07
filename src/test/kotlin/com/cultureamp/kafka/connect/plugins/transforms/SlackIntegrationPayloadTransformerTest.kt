4ae8aa3289d0f3841fbc977d53f40724ada3af0e
davidwheeler123 <david.wheeler@cultureamp.com>
2025-10-07 14:01:10 +1100

    Revert "Merge pull request #116 from cultureamp/chore/changeset-releases"
    This reverts commit 62e18ae5afc2952a074c5e351d93ae2c26b9af8f, reversing
    changes made to 3bdc4219be90ab888fef3d07804287c5941348d2.
    
    This repository is public, and isn't set up to be able to publish to the github repository. However there are things that depend on this repository being public. So for now at least, I will simply undo the changes to automatically release with changesets

--- src/test/kotlin/com/cultureamp/kafka/connect/plugins/transforms/SlackIntegrationPayloadTransformerTest.kt
+++ src/test/kotlin/com/cultureamp/kafka/connect/plugins/transforms/SlackIntegrationPayloadTransformerTest.kt
@@ -9,7 +9,7 @@ import com.mongodb.kafka.connect.util.ConfigHelper
 import org.apache.kafka.connect.data.SchemaAndValue
 import org.apache.kafka.connect.data.Struct
 import org.apache.kafka.connect.source.SourceRecord
-import org.junit.jupiter.api.BeforeEach
+import org.junit.Before
 import java.io.File
 import java.nio.file.Files
 import kotlin.test.Test
@@ -18,7 +18,7 @@ import kotlin.test.assertEquals
 class SlackIntegrationPayloadTransformerTest {
     private lateinit var transformer: SlackIntegrationPayloadTransformer<SourceRecord>
 
-    @BeforeEach
+    @Before
     fun setUp() {
         transformer = SlackIntegrationPayloadTransformer()
     }
