d8d0af10f01d0b1e666eaf1ea167659e465d1146
David Wheeler <david.wheeler@cultureamp.com>
2025-10-08 13:29:37 +1100

    fix: revert changesets releases (#146)
    * Revert "Merge pull request #116 from cultureamp/chore/changeset-releases"
    
    This reverts commit 62e18ae5afc2952a074c5e351d93ae2c26b9af8f, reversing
    changes made to 3bdc4219be90ab888fef3d07804287c5941348d2.
    
    This repository is public, and isn't set up to be able to publish to the github repository. However there are things that depend on this repository being public. So for now at least, I will simply undo the changes to automatically release with changesets
    
    * fix: switch to corretto, update github action versions
    
    * fix: Try disabling gradle cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,1 @@
-apply(from = "get-name-and-version.gradle.kts")
-
-rootProject.name = gradle.extra["package_name"]!! as String
-
-// Automatically downloads the JDK to use for compiling
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0") }
+rootProject.name = "kafka-connect-plugins"
